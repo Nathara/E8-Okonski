feff8bcbdfd77aac6c926b42b62a4f1747383142
Nathara <57373436+Nathara@users.noreply.github.com>
2021-11-02 21:01:31 -0500

    Create TestComputation_CsvFileSource.java

---
+++ junit/q2/src/TestComputation_CsvFileSource.java
@@ -0,0 +1,19 @@
+package q2.csvfilesource;
+
+import org.junit.Assert;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class TestComputation_CsvFileSource {
+
+   @ParameterizedTest
+   @CsvFileSource(resources = "computation_dataset.csv")
+   public void testGetDiscount(int int1, int int2, int exp) throws Exception {
+
+	  Computation com = new Computation();
+      int result = 0; 
+      result = com.getDiscount(int1, int2);
+      Assert.assertEquals(result, result);
+
+   }
+}
